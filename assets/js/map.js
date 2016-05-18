69e492af818ef2fb52b504b67a4cc5748257dc6c
caitlinpe <c.pannellevans@gmail.com>
2016-05-17 22:31:26 -0400

    changes to map scroll js

--- assets/js/map.js
+++ assets/js/map.js
@@ -3,6 +3,12 @@ function initMap() {
     var  mapDiv = document.getElementById("map");
     var map = new google.maps.Map(mapDiv, {
         center: { lat: 51.0486, lng: -114.0708 },
-        zoom: 10
+        zoom: 10,
+        draggable: false,
+        zoomControl: false,
+        scaleControl: false,
+        scrollwheel: false,
+        disableDoubleClickZoom: true,
+        mapTypeControl: false
     });
 }
