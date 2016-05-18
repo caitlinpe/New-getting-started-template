ab899bee19bec44cf32fafd80a7dda4fc961394a
caitlinpe <c.pannellevans@gmail.com>
2016-05-17 23:32:45 -0400

    added location permissions to js

--- assets/js/map.js
+++ assets/js/map.js
@@ -44,4 +44,63 @@ $(document).ready(function() {
 
     });
 
+    if (navigator.geolocation) {
+        getCurrentLocation();
+    } else {
+        console.error("Geolocation API not avilable in this browser");
+    }
+
 });
+
+function getCurrentLocation() {
+    //use HTML5 geolocation API to get current position
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback, { timeout : 10000 });
+}
+
+function successCallback(result) {
+    var lat = result.coords.latitude;
+    var lng = result.coords.longitude;
+    var myLatLng = new google.maps.LatLng (lat, lng);
+    var mapOptions = {
+        center: myLatLng,
+        zoom: 10,
+        draggable: false,
+        zoomControl: false,
+        scaleControl: false,
+        scrollwheel: false,
+        disableDoubleClickZoom: true,
+        mapTypeControl: false
+    };
+
+    var map = new google.maps.Map($("#map").get(0), mapOptions);
+
+    //add marker
+    new google.maps.Marker({
+        position:myLatLng,
+        map: map,
+        title: "My Current Location"
+    })
+
+}
+
+
+function consoleCallback(error) {
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                alert("User location permission denied.");
+                break;
+            case error.POSITION_UNAVAILABLE:
+                alert("User location unavailable.");
+                break;
+            case error.PERMISSION_DENIED_TIMEOUT:
+                alert("User took too long to grant/deny permission.");
+                break;
+            case error.UNKNOWN_ERROR:
+                alert("An unknown Error Occured.");
+                break;
+            default:
+                alert("An unknown Error Occured.");
+                break;
+
+    }
+}
